fcdbb3d0da16bb61e3b74c72c7fcb6127746e48a
iamarshiya <202301070209@mitaoe.ac.in>
2026-02-22 02:30:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Riskmap from "./pages/Riskmap";
 import Forecast from "./pages/Forecast";
-import Locality from "./pages/location";
-import Mitigation from "./pages/Mitigation";
-import about from "./pages/about";
 import Contact from "./pages/Contact";
-import Feature from "./pages/Feature";
 import Chatbot from "./components/chatbot";
 
 import "./App.css";
@@ -20,13 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<about />} />
         <Route path="/riskmap" element={<Riskmap />} />
         <Route path="/forecast" element={<Forecast />} />
-        <Route path="/locality" element={<Locality />} />
-        <Route path="/mitigation" element={<Mitigation />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/feature" element={<Feature />} />
         <Route path="/chatbot" element={<Chatbot />} />
       </Routes>
     </BrowserRouter>
