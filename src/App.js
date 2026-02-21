9e83cbd07af25bf083a41e6300926266eada9f25
asthatayade234015-design <asthatayade234015@gmail.com>
2026-02-21 20:25:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Riskmap from "./pages/Riskmap";
 import Forecast from "./pages/Forecast";
 import Locality from "./pages/Locality";
 import Mitigation from "./pages/Mitigation";
+import about from "./pages/about";
 import Chatbot from "./components/chatbot";
 
 import "./App.css";
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<Mitigation />} />
         <Route path="/riskmap" element={<Riskmap />} />
         <Route path="/forecast" element={<Forecast />} />
         <Route path="/locality" element={<Locality />} />
